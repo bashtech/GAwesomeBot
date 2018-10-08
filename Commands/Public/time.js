18b01c850ab30c29d7582cd00ff9fe57aad75770
Andrew Bashore <andrew@bashtech.net>
2018-10-08 14:24:35 -0400

    Added Google Maps API key

--- Commands/Public/time.js
+++ Commands/Public/time.js
@@ -1,13 +1,14 @@
 const unirest = require("unirest");
 const moment = require("moment");
+const auth = require("./../../Configuration/auth.json");
 
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix, commandData) => {
 	const getTime = (location, member, otherusr) => {
 		if(location) {
-			unirest.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}`).header("Accept", "application/json").end(res => {
+			unirest.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${auth.tokens.google_maps_api_key}&address=${encodeURIComponent(location)}`).header("Accept", "application/json").end(res => {
 				if(res.status==200 && res.body.results.length>0) {
 					location = res.body.results[0].formatted_address;
-					unirest.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${res.body.results[0].geometry.location.lat},${res.body.results[0].geometry.location.lng}&timestamp=${Math.floor(Date.now() / 1000)}&sensor=false`).header("Accept", "application/json").end(res => {
+					unirest.get(`https://maps.googleapis.com/maps/api/timezone/json?key=${auth.tokens.google_maps_api_key}&location=${res.body.results[0].geometry.location.lat},${res.body.results[0].geometry.location.lng}&timestamp=${Math.floor(Date.now() / 1000)}&sensor=false`).header("Accept", "application/json").end(res => {
 						const date = new Date(Date.now() + (parseInt(res.body.rawOffset) * 1000) + (parseInt(res.body.dstOffset) * 1000));
 						msg.channel.createMessage(`🕐 It's ${moment(date).utc().format(config.moment_date_format).replaceAll(" at ", " ")} ${member ? (`for @${bot.getName(msg.channel.guild, serverDocument, member)}`) : (`in ${location}`)} (${res.body.timeZoneName})`);
 					});
